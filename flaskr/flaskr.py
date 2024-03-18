4ba1b1576632b649dda26128b5af4332b8a29f35
Christian  Baun <baunbaun@gmail.com>
2024-03-18 07:52:09 +0100

    Now we persist and show date.

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -59,7 +59,7 @@ def close_db(error):
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    cur = db.execute('SELECT title, text, created_at FROM entries ORDER BY id DESC')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
@@ -69,7 +69,7 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+    db.execute('INSERT INTO entries (title, text, created_at) VALUES (?, ?, CURRENT_DATE)',
                [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
